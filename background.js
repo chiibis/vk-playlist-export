42828d1cb0a4a2144272ee8dcf3b27684a3edae5
Sergey Chizhikov <chiibis@yandex.ru>
2019-03-17 00:31:34 +0300

    Extension Framework with stubs

---
+++ background.js
@@ -0,0 +1,5 @@
+// chrome.browserAction.onClicked.addListener(function() {
+//     chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
+//         chrome.tabs.update( { "url": "https://adtt.actiplans.com/adtt/user/my_schedule.do" } );
+//     });
+// });
---
+++ content/audios_view.js
@@ -0,0 +1,12 @@
+function addExportIcon() {
+    console.log('VK Extension:: addExportIcon()')
+
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+
+    console.log('VK Extension:: DOM content loaded');
+
+    addExportIcon();
+
+});
